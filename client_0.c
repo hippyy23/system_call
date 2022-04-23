7e0c4605fb75288a0b68ff50eaabe9b7ad42d593
Adrian Munteanu <hippyy2323@gmail.com>
2022-04-23 17:28:58 +0200

    Update client_0.c

--- client_0.c
+++ client_0.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <limits.h>
 
+#include <fcntl.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -19,10 +20,10 @@ int main(int argc, char * argv[]) {
         printf("Usage: %s path\n", argv[0]);
         return 0;
     }
-    strncpy(g_wd, argv[1], NAME_MAX);
+    g_wd = argv[1];
 
-    // allocate memory for MAX_FILES files
-    alloc_mem_files();
+    // create a 2d array to contain the path of MAX_FILES files
+    char files[MAX_FILES][NAME_MAX];
 
     // create a mask to block all signals but SIGINT and SIGUSR1
     // set of signals
@@ -37,21 +38,34 @@ int main(int argc, char * argv[]) {
 
     // if SIGUSR1 is recieved client_0 finishes
     // if SIGINT is recieved client_0 starts
-    if (signal(SIGUSR1, quit) == SIG_ERR ||
-        signal(SIGINT, start) == SIG_ERR)
+    if (signal(SIGUSR1, terminate_client0) == SIG_ERR ||
+        signal(SIGINT, start_client0) == SIG_ERR)
         ErrExit("change signal handler failed");
 
+    // wait for a signal to be recieved
     printf("Waiting for a signal...\n");
     pause();
 
-    // search all files that start with "send_"
-    search(0);
+    // search for all files whose name starts with "send_"
+    search(files, 0);
     // get the number of files found
-    int numFiles = get_num_files();
+    int numFiles = get_num_files(files);
     printf("<Client_0> Numero di file trovati %d\n", numFiles);
 
-    // free allocated memory
-    free_mem_files();
+    // send the number of files to the server through fifo1
+    printf("<Client_0> opening fifo1 %s\n", fifo1);
+    int fifo1FD = open(fifo1, O_WRONLY);
+    if (fifo1FD == -1) {
+        ErrExit("open failed");
+    }
+
+    if (write(fifo1FD, &numFiles, sizeof(numFiles)) != sizeof(numFiles)) {
+        ErrExit("write failed");
+    }
+
+    if (close(fifo1FD) != 0) {
+        ErrExit("close failed");
+    }
 
     return 0;
 }
