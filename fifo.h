9b36f3a973511c7c1229a50b24e1677330def706
Adrian Munteanu <hippyy2323@gmail.com>
2022-05-09 21:41:10 +0200

    Delete fifo.h

--- fifo.h
+++
@@ -1,9 +0,0 @@
-/// @file fifo.h
-/// @brief Contiene la definizioni di variabili e
-///         funzioni specifiche per la gestione delle FIFO.
-
-#pragma once
-
-
-void make_fifo(char *);
-void close_fifo(int, char *);
